3f5e6cc478010c932fe158edb69f8942ecf83c47
DasariPadma <paddudasari799@gmail.com>
2023-12-15 10:45:55 +0000

    add maven project

---
+++ src/main/java/com/example/demo.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.ResourceBundle;
+
+public class demo {
+
+	public int userLogin(String in_user , String in_pswd) {
+		ResourceBundle rb = ResourceBundle.getBundle("config");
+		String userName = rb.getString("userName");
+		String password = rb.getString("password");
+		if (in_user.equals(userName) && in_pswd.equals(password))
+			return 1;
+		else
+			return 0;
+	}
+	
+}
